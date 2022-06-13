91e3694cd60fd12289923e8e6c86ef3ee13bf7f6
ChihTeHsu <85528762+ChihTeHsu@users.noreply.github.com>
2022-06-13 15:35:26 -0500

    Add files via upload

---
+++ untitled4.py
@@ -0,0 +1,167 @@
+#from IPython import get_ipython
+#get_ipython().run_line_magic('reset', '-f')
+
+import tensorflow as tf
+from tensorflow import keras
+
+import os
+import tempfile
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import sklearn
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+file = tf.keras.utils
+raw_df = pd.read_csv('creditcard.csv')
+
+neg, pos = np.bincount(raw_df['Class'])
+total = neg + pos
+print('Examples:\n    Total: {}\n    Positive: {} ({:.2f}% of total)\n'.format(total, pos, 100 * pos / total))
+
+#%%
+cleaned_df = raw_df.copy()
+
+# You don't want the `Time` column.
+cleaned_df.pop('Time')
+
+# The `Amount` column covers a huge range. Convert to log-space.
+eps = 0.001 # 0 => 0.1¢
+cleaned_df['Log Amount'] = np.log(cleaned_df.pop('Amount')+eps)
+
+# Use a utility from sklearn to split and shuffle your dataset.
+train_df, test_df = train_test_split(cleaned_df, test_size=0.2)
+train_df, val_df = train_test_split(train_df, test_size=0.2)
+
+# Form np arrays of labels and features.
+train_labels = np.array(train_df.pop('Class'))
+bool_train_labels = train_labels != 0
+val_labels = np.array(val_df.pop('Class'))
+test_labels = np.array(test_df.pop('Class'))
+
+train_features = np.array(train_df)
+val_features = np.array(val_df)
+test_features = np.array(test_df)
+
+#%%
+scaler = StandardScaler()
+train_features = scaler.fit_transform(train_features)
+
+val_features = scaler.transform(val_features)
+test_features = scaler.transform(test_features)
+
+train_features = np.clip(train_features, -5, 5)
+val_features = np.clip(val_features, -5, 5)
+test_features = np.clip(test_features, -5, 5)
+
+
+print('Training labels shape:', train_labels.shape)
+print('Validation labels shape:', val_labels.shape)
+print('Test labels shape:', test_labels.shape)
+
+print('Training features shape:', train_features.shape)
+print('Validation features shape:', val_features.shape)
+print('Test features shape:', test_features.shape)
+
+#%%
+pos_df = pd.DataFrame(train_features[ bool_train_labels], columns=train_df.columns)
+neg_df = pd.DataFrame(train_features[~bool_train_labels], columns=train_df.columns)
+
+sns.jointplot(x=pos_df['V5'], y=pos_df['V6'],
+              kind='hex', xlim=(-5,5), ylim=(-5,5))
+plt.suptitle("Positive distribution")
+
+sns.jointplot(x=neg_df['V5'], y=neg_df['V6'],
+              kind='hex', xlim=(-5,5), ylim=(-5,5))
+_ = plt.suptitle("Negative distribution")
+
+#%%
+METRICS = [
+      keras.metrics.TruePositives(name='tp'),
+      keras.metrics.FalsePositives(name='fp'),
+      keras.metrics.TrueNegatives(name='tn'),
+      keras.metrics.FalseNegatives(name='fn'), 
+      keras.metrics.BinaryAccuracy(name='accuracy'),
+      keras.metrics.Precision(name='precision'),
+      keras.metrics.Recall(name='recall'),
+      keras.metrics.AUC(name='auc'),
+      keras.metrics.AUC(name='prc', curve='PR'), # precision-recall curve
+]
+
+def make_model(metrics=METRICS, output_bias=None):
+  if output_bias is not None:
+    output_bias = tf.keras.initializers.Constant(output_bias)
+  model = keras.Sequential([
+      keras.layers.Dense(
+          16, activation='relu',
+          input_shape=(train_features.shape[-1],)),
+      keras.layers.Dropout(0.5),
+      keras.layers.Dense(1, activation='sigmoid',
+                         bias_initializer=output_bias),
+  ])
+
+  model.compile(
+      optimizer=keras.optimizers.Adam(learning_rate=1e-3),
+      loss=keras.losses.BinaryCrossentropy(),
+      metrics=metrics)
+
+  return model
+
+#%%
+EPOCHS = 100
+BATCH_SIZE = 2048
+
+early_stopping = tf.keras.callbacks.EarlyStopping(
+    monitor='val_prc', 
+    verbose=1,
+    patience=10,
+    mode='max',
+    restore_best_weights=True)
+
+model = make_model()
+model.summary()
+
+baseline_history = model.fit(
+    train_features,
+    train_labels,
+    batch_size=BATCH_SIZE,
+    epochs=EPOCHS,
+    callbacks=[early_stopping],
+    validation_data=(val_features, val_labels))
+
+#%%
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+def plot_metrics(history):
+  metrics = ['loss', 'prc', 'precision', 'recall']
+  for n, metric in enumerate(metrics):
+    name = metric.replace("_"," ").capitalize()
+    plt.subplot(2,2,n+1)
+    plt.plot(history.epoch, history.history[metric], color=colors[0], label='Train')
+    plt.plot(history.epoch, history.history['val_'+metric],
+             color=colors[0], linestyle="--", label='Val')
+    plt.xlabel('Epoch')
+    plt.ylabel(name)
+    if metric == 'loss':
+      plt.ylim([0, plt.ylim()[1]])
+    elif metric == 'auc':
+      plt.ylim([0.8,1])
+    else:
+      plt.ylim([0,1])
+
+    plt.legend();
+
+plot_metrics(baseline_history)
+
+#%%
+baseline_results = model.evaluate(test_features, test_labels,
+                                  batch_size=BATCH_SIZE, verbose=0)
+for name, value in zip(model.metrics_names, baseline_results):
+  print(name, ': ', value)
+print()
